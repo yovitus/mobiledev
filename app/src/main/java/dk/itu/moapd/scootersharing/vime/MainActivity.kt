13c8d797a5dd32a4f3559465ec4c0d0eaf10af14
yovitus <vime@itu.dk>
2023-02-07 08:15:40 +0100

    init

---
+++ app/src/main/java/dk/itu/moapd/scootersharing/vime/MainActivity.kt
@@ -0,0 +1,11 @@
+package dk.itu.moapd.scootersharing.vime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
