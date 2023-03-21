b6c706ba534546569e5469cb8eb0881034bd43a1
Max <maxt@itu.dk>
2023-03-21 12:48:39 +0100

    Add instrumental testing

--- app/src/androidTest/java/dk/itu/moapd/scootersharing/vime/activities/MainActivityTest.kt
+++ app/src/androidTest/java/dk/itu/moapd/scootersharing/vime/activities/MainActivityTest.kt
@@ -4,9 +4,9 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.*
 import dk.itu.moapd.scootersharing.vime.R
@@ -38,5 +38,29 @@ class MainActivityTest {
 
         onView(withId(R.id.recycler_view))
             .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun startRideAddsRideToRecyclerView() {
+        onView(withId(R.id.recycler_view))
+            .check(matches(hasChildCount(3)))
+
+        onView(withId(R.id.start_ride_button))
+            .perform(click())
+
+        onView(withId(R.id.edit_text_name))
+            .perform(typeText("testScooter"))
+
+        onView(withId(R.id.edit_text_location))
+            .perform(typeText("testLocation"))
+
+        onView(withId(R.id.start_ride_button))
+            .perform(click())
+
+        onView(withText("OK"))
+            .perform(click())
+
+        onView(withId(R.id.recycler_view))
+            .check(matches(hasChildCount(4)))
     }
 }
