49db2332d16ca04ba69b2f885f146dbb2d94bc99
Max <maxt@itu.dk>
2023-03-29 23:32:59 +0200

    Change waitForView to an extension function for Matcher<View>

--- app/src/androidTest/java/dk/itu/moapd/scootersharing/vime/activities/MainActivityTest.kt
+++ app/src/androidTest/java/dk/itu/moapd/scootersharing/vime/activities/MainActivityTest.kt
@@ -1,17 +1,15 @@
 package dk.itu.moapd.scootersharing.vime.activities
 
-import android.view.View
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.NoMatchingViewException
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import dk.itu.moapd.scootersharing.vime.R
-import org.hamcrest.Matcher
+import dk.itu.moapd.scootersharing.vime.utils.awaitView
 import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
@@ -23,50 +21,20 @@ import org.junit.runner.RunWith
 class MainActivityTest {
     // companion object is for @BeforeClass annotations
     companion object {
-
         /**
-         * Helper function for setUpAll(). Waits until a specified view is visible.
-         * @param view The view that the function waits for.
-         * @param delayMillis Millisecond delay between each check if it has become visible.
-         * @param maxTries Amount of tries checking if it has become visible, will throw exception
-         * when tries exceed maxTries.
-         */
-        private fun waitForView(view: Matcher<View>, delayMillis: Long=500, maxTries: Int=5) {
-            var viewNotFound = true
-            var tries = 0
-            while (viewNotFound) {
-                Thread.sleep(delayMillis)
-
-                try {
-                    onView(view)
-                    viewNotFound = false
-                } catch (e: NoMatchingViewException) {
-                    tries++
-                    if (tries > maxTries) {
-                        throw e
-                    }
-                }
-            }
-        }
-
-        /**
-         * setUpAll() is for logging in to a test user.
-         * @BeforeClass @JvmStatic ensures that this function is called once
-         * before all
+         * setUpAll() is for logging in to a test user. @BeforeClass @JvmStatic ensures that this function is called once
+         * before all tests.
          */
         @BeforeClass @JvmStatic
         fun setUpAll() {
             val loginScenario = launch(LoginActivity::class.java)
 
-            waitForView(withText("Sign in with email"))
-            onView(withText("Sign in with email")).perform(click())
+            onView(withText("Sign in with email").awaitView()).perform(click())
 
-            waitForView(withHint("Email"))
-            onView(withHint("Email")).perform(typeText("john@doe.com"))
+            onView(withHint("Email").awaitView()).perform(typeText("john@doe.com"))
             onView(withText("NEXT")).perform(click())
 
-            waitForView(withHint("Password"))
-            onView(withHint("Password")).perform(typeText("123456"))
+            onView(withHint("Password").awaitView()).perform(typeText("123456"))
             onView(withText("SIGN IN")).perform(click())
 
             loginScenario.close()
