4fc86d6ba4d2f22e018725ff54b3015e41249aa5
Max <maxt@itu.dk>
2023-04-25 13:14:02 +0200

    Unfinished business

--- app/src/main/java/dk/itu/moapd/scootersharing/vime/fragments/MapsFragment.kt
+++ app/src/main/java/dk/itu/moapd/scootersharing/vime/fragments/MapsFragment.kt
@@ -12,11 +12,25 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Looper
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat.checkSelfPermission
+import com.google.android.gms.location.*
 import dk.itu.moapd.scootersharing.vime.R
 import dk.itu.moapd.scootersharing.vime.utils.getScooters
 import kotlinx.coroutines.*
 
 class MapsFragment : Fragment() {
+
+    companion object {
+        private const val ALL_PERMISSION_RESULTS = 1011
+        private var
+    }
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
 
     private val database =
         Firebase.database("https://scooter-sharing-6a9a7-default-rtdb.europe-west1.firebasedatabase.app/").reference
@@ -40,6 +54,7 @@ class MapsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+//        startLocationAware()
         return inflater.inflate(R.layout.fragment_maps, container, false)
     }
 
@@ -48,4 +63,88 @@ class MapsFragment : Fragment() {
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
     }
+
+    override fun onResume() {
+        super.onResume()
+        subscribeToLocationUpdates()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unsubscribeToLocationUpdates()
+    }
+
+    private fun startLocationAware() {
+        requestUserPermissions()
+
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(this.requireActivity())
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult) {
+                super.onLocationResult(p0)
+
+                p0.lastLocation?.let { location ->
+                    setAddress(location)
+                }
+            }
+        }
+    }
+
+    private fun requestUserPermissions() {
+        val permissions: ArrayList<String> = ArrayList()
+
+        permissions.add(Manifest.permission.ACCESS_FINE_LOCATION)
+        permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION)
+
+        val permissionsToRequest = permissionsToRequest(permissions)
+
+        if (permissionsToRequest.size > 0) {
+            requestPermissions(
+                permissionsToRequest.toTypedArray(),
+                ALL_PERMISSION_RESULTS
+            )
+        }
+    }
+
+    private fun permissionsToRequest(permissions: ArrayList<String>): ArrayList<String> {
+        val result: ArrayList<String> = ArrayList()
+
+        for (permission in permissions)
+            if (checkSelfPermission(
+                    requireContext(),
+                    permission
+                ) != PackageManager.PERMISSION_GRANTED
+            )
+                result.add(permission)
+        return result
+    }
+
+    private fun checkPermission() =
+        ActivityCompat.checkSelfPermission(
+            requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(
+                    requireActivity(), Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+
+    private fun subscribeToLocationUpdates() {
+        if (checkPermission())
+            return
+
+        val locationRequest = LocationRequest
+            .Builder(Priority.PRIORITY_HIGH_ACCURACY, 5)
+            .build()
+
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest, locationCallback, Looper.getMainLooper()
+        )
+    }
+
+    private fun unsubscribeToLocationUpdates() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+    }
+
+    private fun setAddress
+
 }
