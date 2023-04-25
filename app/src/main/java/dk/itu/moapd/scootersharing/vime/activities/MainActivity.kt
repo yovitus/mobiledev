daddea99ffc255b50d55eef7a339627737bdc86d
Max <maxt@itu.dk>
2023-04-25 13:26:50 +0200

    Fix shit

--- app/src/main/java/dk/itu/moapd/scootersharing/vime/activities/MainActivity.kt
+++ app/src/main/java/dk/itu/moapd/scootersharing/vime/activities/MainActivity.kt
@@ -1,24 +1,13 @@
 package dk.itu.moapd.scootersharing.vime.activities
 
 import android.content.Intent
-import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
-//import com.google.firebase.database.DatabaseReference
-//import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
 import dk.itu.moapd.scootersharing.vime.R
 import dk.itu.moapd.scootersharing.vime.databinding.ActivityMainBinding
 
@@ -28,21 +17,17 @@ import dk.itu.moapd.scootersharing.vime.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     companion object {
         private val TAG = MainActivity::class.qualifiedName
-        //lateinit var database: DatabaseReference
     }
 
     /**
      * View binding allows easy written code to interact with views.
      */
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+
     /**
      * Authentication for logged in user. If auth.currentUser == null, navigate to LoginActivity.
      */
     private lateinit var auth: FirebaseAuth
-
-//    private lateinit var navGraph: NavController
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
 
     /**
      * onCreate is called when the activity starts. Initialization such as `setContentView(view)`
@@ -56,36 +41,13 @@ class MainActivity : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
 //        navGraph = findNavController(R.id.nav_graph)
         binding = ActivityMainBinding.inflate(layoutInflater)
-//        binding.bottomNav.setOnItemSelectedListener { item ->
-//            when (item.itemId) {
-//                R.id.home -> {
-//                    navGraph.navigate(R.id.home)
-//                    true
-//                } R.id.maps -> {
-//                    navGraph.navigate(R.id.maps)
-//                    true
-//                } R.id.profile -> {
-//                    navGraph.navigate(R.id.profile)
-//                    true
-//                }
-//                else -> false
-//            }
-//        }
         val view = binding.root
         setContentView(view)
         val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.frament_container) as NavHostFragment
+            .findFragmentById(R.id.fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
 
-        // Setup the action bar only in the portrait mode.
-        if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
-            appBarConfiguration = AppBarConfiguration(navController.graph)
-            setupActionBarWithNavController(navController, appBarConfiguration)
-        }
-
-//        database =
-//            Firebase.database("https://scooter-sharing-6a9a7-default-rtdb.europe-west1.firebasedatabase.app/").reference
-
+        binding.bottomNav.setupWithNavController(navController)
     }
 
 
@@ -101,29 +63,9 @@ class MainActivity : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        //add navigation to mainFragment, mapsFragment and startRideFragment using actions
-//        when (item.itemId) {
-//            R.id.home -> {
-//                Navigation.findNavController(binding.root).navigate(R.id.action_mapsFragment_to_mainFragment)
-//                true
-//            } R.id.maps -> {
-//                Navigation.findNavController(binding.root).navigate(R.id.action_mainFragment_to_mapsFragment)
-//                true
-//            } R.id.profile -> {
-//                Navigation.findNavController(binding.root).navigate(R.id.action_mainFragment_to_startRideFragment)
-//                true
-//            }
-//            else -> false
-//        }
-//        return super.onOptionsItemSelected(item)
-//    }
-
-
     private fun startLoginActivity() {
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
         finish()
     }
-
 }
