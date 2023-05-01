738b0e5f7d6f8b77df443c75d324ff18150f17b1
Max <maxt@itu.dk>
2023-05-01 17:00:47 +0200

    Merge from main

--- app/src/main/java/dk/itu/moapd/scootersharing/vime/activities/MainActivity.kt
+++ app/src/main/java/dk/itu/moapd/scootersharing/vime/activities/MainActivity.kt
@@ -39,7 +39,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         auth = FirebaseAuth.getInstance()
-        startCurrentRideActivity()
+//        startCurrentRideActivity()
         if (auth.currentUser == null)
             startLoginActivity()
         binding = ActivityMainBinding.inflate(layoutInflater)
